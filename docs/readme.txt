ebbf8b1420594159351d80f4706e8993b675feca
yangjiandong <young.jiandong@gmail.com>
2013-01-21 16:51:24 +0800

    add demo...

---
+++ docs/readme.txt
@@ -0,0 +1,18 @@
+jquery.mobile
+=============
+
+2013.01.21
+----------
+
+    1. icecream
+    http://i-am.ws/entry/jquery_mobile_on_android
+
+    2. slidemenu
+    https://github.com/tegansnyder/JQuery-Mobile-Slide-Menu
+
+2012.12.03
+----------
+
+    1. http://mobile.tutsplus.com/tutorials/mobile-web-apps/jquery_android/
+
+    --END
--- src/com/news/NewsActivity.java
+++ src/com/news/NewsActivity.java
@@ -7,32 +7,36 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class NewsActivity extends Activity {
-	WebView mWebView;
+    WebView mWebView;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
+
         mWebView = (WebView) findViewById(R.id.webview);
         mWebView.setWebViewClient(new NewsClient());
         mWebView.getSettings().setJavaScriptEnabled(true);
         mWebView.getSettings().setDomStorageEnabled(true);
-        mWebView.loadUrl("file:///android_asset/www/index.html");
+        //mWebView.loadUrl("file:///android_asset/www/index.html");
+        mWebView.loadUrl("file:///android_asset/icecream/index.html");
+        //mWebView.loadUrl("file:///android_asset/demos/index.html");
+
+        //mWebView.loadUrl("file:///android_asset/slidemenu/index.html");
     }
 
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {
-            mWebView.goBack();            
+            mWebView.goBack();
             return true;
         }
         return super.onKeyDown(keyCode, event);
     }
-   
+
     private class NewsClient extends WebViewClient {
-    	
+
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
-        	System.out.println("URL: " + url);
-        	view.loadUrl("javascript:changeLocation('" + url + "')");
+            System.out.println("URL: " + url);
+            view.loadUrl("javascript:changeLocation('" + url + "')");
             return true;
         }
     }
